98da261c2183437124ca8a0bae733ab481670406
Mark Vayngrib <mvayngrib@gmail.com>
2020-05-19 12:35:20 -0400

    unpublish android (#1)
    * unpublish android
    
    * 3.1.2-no-android.0
    
    * fix main field
    
    * 3.1.2-no-android.1
    
    * fix import
    
    * 3.1.2-no-android.2

---
+++ index.android.js
@@ -0,0 +1,10 @@
+const thrower = () => {
+  throw new Error('not available on Android yet!')
+}
+
+const PlaidLink = () => null
+
+export default PlaidLink
+
+export const openLink = thrower
+export const dismissLink = thrower
